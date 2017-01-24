244a4686d3e65b5dd8b31e9acff3f4b930cad43a
Mckinley <mckinley.s.hill@gmail.com>
2017-01-24 16:32:05 +0000

    add functionality

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.send('HELLO WORLD!!!!');
-    
+app.get('/:timestamp', function(req, res){
+    var timestamp = req.params.timestamp;
+    res.send('Timestamp: '+ timestamp);
+    res.json(getTimestampJSON(timestamp));
 });
+    
+
 
 app.listen(PORT, function(){
     console.log('server is listening on port ' + PORT);
 });
+
+function getTimestampJSON(timestamp){
+    var result = {
+        unix: null,
+        natural: null
+    };
+    
+    var date;
+    if(!isNAN(parseInt(timestamp))){
+        date = new Date(parseInt(timestamp));
+    }else{
+        date = new Date(timestamp)
+    }
+    
+    if(!isNAN(date.getTime())){
+        
+        result.unix = date.getTime();
+        result.natural = 'in due time';
+    }
+    
+    return result;
+}
